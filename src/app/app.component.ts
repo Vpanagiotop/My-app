ca96bc4c7931ff52e3c194fb63e6d5b7441687ed
Vasilis Panagiotopoulos <vasilis.panagiotop@gmail.com>
2022-01-14 20:45:54 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'My-app';
+export class AppComponent implements OnInit {
+  public dx = 2;
+  public data = new BehaviorSubject([] as any[]);
+  public async ngOnInit() {
+    const response = await fetch('http://localhost:5000/weatherforecast');
+    const data = await response.json();
+    console.log(data);
+    this.data.next(data);
+  }
+  public sayHello() {
+    alert('hello');
+  }
 }
