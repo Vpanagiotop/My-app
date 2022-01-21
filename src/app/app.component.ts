34ffea9a6f153bbf98df810d7d48025627d0bd34
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2022-01-22 01:04:55 +0200

    Abstracted panels into their own component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-// import { KeyObject, VerifyKeyObjectInput } from 'crypto';
+import { Component, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 const DEFAULT_OPTION = '--- Select ---';
@@ -9,8 +8,7 @@ const DEFAULT_OPTION = '--- Select ---';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
-  private readonly PANEL_OPEN_CLASS = 'open';
+export class AppComponent implements OnInit {
   public panels = ['active', 'active', 'active'];
   public list: any;
   public selected: any;
@@ -50,78 +48,8 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.kosmas.next(await response3.json());
     console.log(this.kosmas);
   }
-  /**
-   * This function is like ngOnInit, but it runs AFTER the UI has been drawn
-   * for the first time, so that we know how much height the .panel-content has.
-   */
-  public ngAfterViewInit() {
-    document.querySelectorAll('.panel-v2').forEach(element => {
-      const panel = element as HTMLElement;
-      this.openPanel(panel);
-      this.enablePanelTransitionNextTick(panel);
-    });
-  }
-  public shrinkContent(i: number) {
-    // Temporary fix because panel-v2 broke this implementation
-    i = i - 1;
-    // alert('hello');
-    const panel = document.getElementsByClassName('panel') as HTMLCollectionOf<HTMLElement>;
-    const btn = document.getElementsByClassName('accordion') as HTMLCollectionOf<HTMLElement>;
-
-    if (this.panels[i] == 'active') {
-      this.panels[i] = 'null';
-      panel[i].style.transition = 'all 0.5s ease-out'
-      panel[i].style.opacity = '0'
-      panel[i].style.maxHeight = '0';
-      panel[i].style.padding = '0px';
-      panel[i].style.overflow = 'hidden';
-      btn[i].classList.remove('active');
-    }
-    else {
-      this.panels[i] = 'active';
-      panel[i].style.maxHeight = 'none';
-      panel[i].style.transition = 'all 0.5s ease-out'
-      panel[i].style.opacity = '1'
-      panel[i].style.padding = '0px 0px 20px 0px';
-      panel[i].style.overflow = 'visible';
-
-      btn[i].classList.toggle('active');
-    }
-  }
-  public togglePanel(event: MouseEvent) {
-    const target = event.target as HTMLElement;
-    const panel = target.nextSibling as HTMLElement;
-
-    if (panel.classList.contains(this.PANEL_OPEN_CLASS)) {
-      this.closePanel(panel);
-    } else {
-      this.openPanel(panel);
-    }
-  }
   public sayHi() {
     alert('hi');
-  }
-  private openPanel(panel: HTMLElement) {
-    const content = panel.querySelector('.panel-content') as HTMLElement;
-    panel.classList.add(this.PANEL_OPEN_CLASS);
-    panel.style.height = content.clientHeight + 'px';
-    panel.previousElementSibling?.classList.add('active');
-  }
-  private closePanel(panel: HTMLElement) {
-    panel.classList.remove(this.PANEL_OPEN_CLASS);
-    panel.style.height = '';
-    panel.previousElementSibling?.classList.remove('active');
-  }
-  /**
-   * We only want transition to be enabled AFTER the panel is first drawn,
-   * otherwise the user would see a transition during page load. To
-   * "schedule" a command to run as soon as possible but not right now, 
-   * we use `setTimeout`.
-   */
-  private enablePanelTransitionNextTick(panel: HTMLElement) {
-    setTimeout(() => {
-      panel.style.transition = 'all 0.5s';
-    });
   }
 };
 
---
+++ src/app/panel/panel.component.ts
@@ -0,0 +1,64 @@
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.scss']
+})
+export class PanelComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('header') public header!: ElementRef<HTMLDivElement>;
+  @ViewChild('content') public content!: ElementRef<HTMLDivElement>;
+  @Input() public title!: string;
+  public state: 'open' | 'closed' = 'open';
+  private resizeObserver?: ResizeObserver;
+  public constructor(private element: ElementRef<HTMLElement>) { }
+  public ngAfterViewInit() {
+    this.updateHeight();
+    this.enableTransitionsOnNextTick();
+    this.registerHeightChangeListener();
+  }
+  public ngOnDestroy() {
+    this.resizeObserver?.disconnect();
+  }
+  public toggle() {
+    if (this.state === 'open') {
+      this.close();
+    } else {
+      this.open();
+    }
+  }
+  private enableTransitionsOnNextTick() {
+    setTimeout(() => {
+      this.element.nativeElement.style.transition = 'all .5s';
+    });
+  }
+  private registerHeightChangeListener() {
+    this.resizeObserver = new ResizeObserver(this.updateHeight.bind(this));
+    this.resizeObserver.observe(this.content.nativeElement);
+  }
+  private updateHeight() {
+    if (this.state === 'open') {
+      const elementHeight = this.getHeaderHeight() + this.getContentHeight();
+      this.setHeight(elementHeight);
+    } else {
+      this.setHeight(this.getHeaderHeight());
+    }
+  }
+  private getHeaderHeight() {
+    return this.header.nativeElement.clientHeight;
+  }
+  private getContentHeight() {
+    return this.content.nativeElement.clientHeight;
+  }
+  private setHeight(height: number) {
+    this.element.nativeElement.style.height = height + 'px';
+  }
+  private open() {
+    this.state = 'open';
+    this.updateHeight();
+  }
+  private close() {
+    this.state = 'closed';
+    this.updateHeight();
+  }
+}
