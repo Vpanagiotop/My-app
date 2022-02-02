6c5aa3c47f8f8f736c101635e23ceeab1627cece
Vasilis Panagiotopoulos <vasilis.panagiotop@gmail.com>
2022-02-02 19:43:21 +0200

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PanelComponent } from './panel/panel.component';
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PanelComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent,
+    NavBarComponent
   ],
   imports: [
     FormsModule,
